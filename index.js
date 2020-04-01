049519d23c4640b675efefae4c454388672f984d
Edith-LE <edith.lop.estrada@gmail.com>
2020-04-01 18:57:33 -0500

    change form, and add icons and styles

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const $emButton = document.getElementById('btn-employee');
 const $hrButton = document.getElementById('btn-hr');
 
 $hrButton.addEventListener ('click', () => {
+    $hrButton.blur()
     $hrSection.style.zIndex ="1"
     $employeeSection.style.zIndex ="-1"
 
 });
 
 $emButton.addEventListener ('click', () =>{
+    $emButton.blur()
     $hrSection.style.zIndex = "-1"
     $employeeSection.style.zIndex = "1"
 })
